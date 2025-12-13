853a46cebac4fd19d4c86315ccb0271c1a4c50fb
xmansx1 <xmansx1122@gmail.com>
2025-12-13 22:39:12 +0300

    1345

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from website import views as website_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("cpanel-123/", admin.site.urls),
@@ -13,6 +14,9 @@ urlpatterns = [
 
     # API Root (كل شيء تحت /api/)
     path("api/", include(("core.api_urls", "core_api"), namespace="core_api")),
+
+
+    path("favicon.ico", RedirectView.as_view(url="/static/favicon.ico", permanent=True)),
 
     # صفحات schedule (لو عندك صفحات غير API)
     path("schedule/", include(("schedule.urls", "schedule"), namespace="schedule")),
